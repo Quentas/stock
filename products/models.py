b4f99f6bcb620040ef5eafdb8dc4289ee70449ef
Quentas <quentas320@gmail.com>
2022-02-04 17:59:44 +0200

    init commit

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(unique=True, blank=False, max_length=20)
+
+
+class Product(models.Model):
+    name = models.CharField(unique=True, blank=False, max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    status = models.CharField(choices=[
+        ('in_stock', 'In stock'),
+        ('out_of_stock', 'Out of stock')
+    ], max_length=12)
+    remains = models.IntegerField()
