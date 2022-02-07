2e0cfc89ae809e98781459e813854b79c8e3778e
Quentas <quentas320@gmail.com>
2022-02-07 18:24:10 +0200

    update: products generating

--- products/management/commands/generate_product.py
+++ products/management/commands/generate_product.py
@@ -19,33 +19,23 @@ class Command(BaseCommand):
             'products', type=int, 
             help=u'Количество создаваемых продуктов в категории')
 
+    @transaction.atomic
     def handle(self, *args, **kwargs):
         category_names = Category.objects.values_list('name', flat=True)
         product_names = Product.objects.values_list('name', flat=True)
         new_category_names = [random_string(category_names) for _ in range(kwargs['categories'])]
         new_product_names = [random_string(product_names) for _ in range(kwargs['products'])]
-        
-        transaction.set_autocommit(False)
-        try:
-            Category.objects.bulk_create(
-                [Category(name=name) for name in new_category_names]
-            )
-            categories = Category.objects.all()
-            Product.objects.bulk_create(
-                [Product(
-                    name=name, 
-                    category=choice(categories),
-                    price=0,
-                    remains=0,
-                    status='out_of_stock')
-                for name in new_product_names
-                ]
-            )
-        except:
-            transaction.rollback()
-            raise
-        else:
-            transaction.commit()
-        finally:
-            transaction.set_autocommit(True)
-        
+        Category.objects.bulk_create(
+            [Category(name=name) for name in new_category_names]
+        )
+        categories = Category.objects.all()
+        Product.objects.bulk_create(
+            [Product(
+                name=name, 
+                category=choice(categories),
+                price=0,
+                remains=0,
+                status='out_of_stock')
+            for name in new_product_names
+            ]
+        )
